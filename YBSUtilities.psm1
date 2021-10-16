f0a2faa8b179e71883f2fe51eed1366ab9a58f50
Ken Mitchell <ken.mitchell@yellowboxsolutions.com>
2021-10-15 22:21:24 -0500

    Catching up

--- YBSUtilities.psm1
+++
@@ -1,9 +0,0 @@
-# Source all functions in the public folder and export
-$Functions = @( Get-ChildItem -Path $PSScriptRoot\functions\*.ps1 -ErrorAction SilentlyContinue )
-foreach ($Import in @($Functions)) {
-    try {
-        . $Import.fullname
-    } catch {
-        throw "Could not import function $($Import.fullname): $_"
-    }
-}
